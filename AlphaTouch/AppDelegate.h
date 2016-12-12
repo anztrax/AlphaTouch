40d8cbd72229a41cba5ddc88ac955ab3273cd320
andrew ananta <kuro.ketanitem@gmail.com>
2016-12-13 02:45:58 +0700

    Initial Commit

---
+++ AlphaTouch/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AlphaTouch
+//
+//  Created by andrew ananta on 12/13/16.
+//  Copyright © 2016 andrew ananta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
