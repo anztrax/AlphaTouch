42a237b757bd9442fb56b70b3699ca8e9a53fdf4
andrew ananta <kuro.ketanitem@gmail.com>
2016-12-13 14:45:10 +0700

    1. add scroll view to profile ViewController 2. add unnecessary label for test the scroll view :p

--- AlphaTouch/ProfileViewController.h
+++ AlphaTouch/ProfileViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ProfileViewController : UIViewController
-
+@property(nonatomic) UIScrollView *scrollView;
 @end
--- AlphaTouch/ProfileViewController.m
+++ AlphaTouch/ProfileViewController.m
@@ -19,10 +19,39 @@
     // Do any additional setup after loading the view.
   self.view.backgroundColor = [UIColor brownColor];
   
+  CGFloat currentWidth = self.view.frame.size.width;
+  
+  self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+  self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleHeight;  //this code is make scrollview height same as the screen view (not the size after cut with tab view)
+  self.scrollView.contentSize = CGSizeMake(round(currentWidth), 1320);
+  
   UIImageView *catProfileImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"profileCatImage"]];
   [catProfileImage setContentMode:UIViewContentModeScaleAspectFit];
-  catProfileImage.frame = self.view.frame;
-  [self.view addSubview:catProfileImage];
+  catProfileImage.frame = CGRectMake(20, 20, 100, 113);
+  [self.scrollView addSubview:catProfileImage];
+  
+  UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 140, 280, 40)];
+  nameLabel.text = @"Name : Nyan Cat";
+  [self.scrollView addSubview:nameLabel];
+  
+  UILabel *cityLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 200, 280, 40)];
+  cityLabel.text = @"From : Indonesia";
+  [self.scrollView addSubview:cityLabel];
+  
+  UILabel *memberSinceLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 260, 280, 40)];
+  memberSinceLabel.text = @"November 2016";
+  [self.scrollView addSubview:memberSinceLabel];
+  
+  UITextView *biography = [[UITextView alloc]initWithFrame:CGRectMake(12, 320, 300, 1000)];
+  biography.font = [UIFont fontWithName:@"Helvetica" size:120];
+  biography.editable = NO;
+  biography.backgroundColor = [UIColor brownColor];
+  biography.scrollEnabled = NO;
+  biography.text = @"Nyan cat is nyan nyan nya nya nya nya nyan ...";
+  [self.scrollView addSubview:biography];
+  
+  [self.view addSubview:self.scrollView];
+  
 }
 
 - (void)didReceiveMemoryWarning {
